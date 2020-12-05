183de99b22eaabf2629d8293f7f7db3159902637
Willardy Tyrone <willardy@gmail.com>
2020-12-04 21:12:57 -0300

    criando testes unitários para o Service de calculadora

--- src/app/calculadora/services/calculadora.service.spec.ts
+++ src/app/calculadora/services/calculadora.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CalculadoraService } from './calculadora.service';
+import {inject} from "@angular/core";
 
 describe('CalculadoraService', () => {
   let service: CalculadoraService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ providers: [CalculadoraService]});
     service = TestBed.inject(CalculadoraService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+
+  it('Deve garantir que 1+4 = 5', () =>{
+    let soma = service.calcular(1,4,CalculadoraService.SOMA);
+    expect(soma).toEqual(5);
+  });
+
+  it('Deve garantir que 4-3 = 1', () =>{
+    let soma = service.calcular(4,3,CalculadoraService.SUBTRACAO);
+    expect(soma).toEqual(1);
+  });
+
+  it('Deve garantir que 4*3 = 12', () =>{
+    let soma = service.calcular(4,3,CalculadoraService.MULTIPLICACAO);
+    expect(soma).toEqual(12);
+  });
+
+  it('Deve garantir que 4/2 = 2', () =>{
+    let soma = service.calcular(4,2,CalculadoraService.DIVISAO);
+    expect(soma).toEqual(2);
+  });
+
+  it('Deve garantir que um operador inválido retorne 0', () =>{
+    let soma = service.calcular(5,3,'%');
+    expect(soma).toEqual(0);
+  });
+
 });
