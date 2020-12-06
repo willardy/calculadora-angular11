d81147581c83057a0a56d532d7c666d15d187d2f
Willardy Tyrone <willardy@gmail.com>
2020-12-06 13:34:52 -0300

    Implementando o componente da calculadora.

--- src/app/calculadora/components/calculadora.component.ts
+++ src/app/calculadora/components/calculadora.component.ts
@@ -7,30 +7,99 @@ import {CalculadoraService} from '../services';
   styleUrls: ['./calculadora.component.css']
 })
 export class CalculadoraComponent implements OnInit {
-  display: any;
+
+  private numero1: string = '0';
+  private numero2: string = 'null';
+  private resultado: number = 0;
+  private operacao: string = 'null';
 
   constructor(private calculadoraService: CalculadoraService) { }
 
   ngOnInit(): void {
+    this.limpar();
   }
 
-  // tslint:disable-next-line:typedef
-  adicionarNumero(s: string) {
+  adicionarNumero(numero: string): void {
+    if(this.operacao === 'null'){
+      this.numero1 = this.concatenarNumero(this.numero1, numero);
+    }else{
+      this.numero2 = this.concatenarNumero(this.numero2, numero)
+    }
+  }
+
+  definirOperacao(operacao: string): void {
+
+    if(this.operacao === 'null'){
+      this.operacao = operacao;
+      return;
+    }
+
+    if(this.numero2 !== 'null'){
+      this.resultado = this.calculadoraService.calcular(
+        parseFloat(this.numero1),
+        parseFloat(this.numero2),
+        this.operacao
+      )
+
+      this.operacao = operacao;
+      this.numero1 = this.resultado.toString();
+      this.numero2 = 'null';
+      this.resultado = 0;
+    }
 
   }
 
-  // tslint:disable-next-line:typedef
-  definirOperacao(s: string) {
-
-  }
-
-  // tslint:disable-next-line:typedef
   calcular() {
 
+
+
+    if(this.numero2 === 'null'){
+      return;
+    }
+
+    this.resultado = this.calculadoraService.calcular(
+      parseFloat(this.numero1),
+      parseFloat(this.numero2),
+      this.operacao);
+
+    console.log(this.numero1)
+    console.log(this.numero2)
+    console.log(this.resultado)
   }
 
-  // tslint:disable-next-line:typedef
-  limpar() {
+  limpar(): void {
+    this.numero1 = '0';
+    this.numero2 = 'null';
+    this.resultado = 0;
+    this.operacao = 'null';
+  }
 
+  concatenarNumero(numAtual: string, numConcatenado: string): string{
+
+    if(numAtual === '0' || numAtual === 'null'){
+      numAtual = '';
+    }
+
+    if(numConcatenado === '.' && numAtual === ''){
+      return '0.';
+    }
+
+    if(numConcatenado === '.' && numAtual.indexOf('.') > -1){
+      return numAtual;
+    }
+
+    return numAtual + numConcatenado;
+  }
+
+  get display(): string{
+    if(this.resultado !== 0){
+      return this.resultado.toString();
+    }
+
+    if(this.numero2 !== 'null'){
+      return this.numero2;
+    }
+
+    return this.numero1;
   }
 }
